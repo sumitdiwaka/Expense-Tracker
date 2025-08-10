e8950a435b67c3ab5fa4e5c48fe7ddc4713894d1
Sumit Diwakar <160616938+sumitdiwaka@users.noreply.github.com>
2025-08-10 21:12:38 +0530

    Update App.jsx

--- Frontend/expenses-tracker/src/App.jsx
+++ Frontend/expenses-tracker/src/App.jsx
@@ -10,7 +10,7 @@ import SignUp from './pages/Auth/SignUp'
 import Home from './pages/Dashboard/Home'
 import Income from './pages/Dashboard/Income'
 import Expense from './pages/Dashboard/Expense'
-import UserProvider from './context/userContext'
+import UserProvider from './context/UserContext'
 import {Toaster} from 'react-hot-toast'
 
 const App = () => {
